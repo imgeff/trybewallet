1d5effd9fe628983caefa160602815a0ed497971
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-01-10 17:12:56 -0300

    req/01: Create management redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
+import { combineReducers } from 'redux';
 // import user from './user';
+import userReducer from './user';
 // import wallet from './wallet';
-
+import walletReducer from './wallet';
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const rootReducer = combineReducers({ userReducer, walletReducer });
+
+export default rootReducer;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;

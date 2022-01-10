39427f168637303675617f79716021838e78a629
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-01-10 18:57:27 -0300

    req/03: recover data of user and render in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function Header() {
-  return (
-    <header>
-      <div>
-        <p data-testid="email-field">teste@teste.com</p>
-      </div>
-      <div>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
-      </div>
-    </header>
-  );
+class Header extends React.Component {
+  render() {
+    const { userEmail } = this.props;
+    return (
+      <header>
+        <div>
+          <p data-testid="email-field">{ userEmail }</p>
+        </div>
+        <div>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </header>
+    );
+  }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);

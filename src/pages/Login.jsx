892e60cf7fe6d070e920f5bddc325ef10f4acc35
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-01-10 16:55:48 -0300

    req/01: Add validation of inputs and redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+      redirect: false,
+    };
+    this.handleChangeLogin = this.handleChangeLogin.bind(this);
+    this.handleClickLogin = this.handleClickLogin.bind(this);
+  }
+
+  handleClickLogin() {
+    this.setState({ redirect: true });
+  }
+
+  handleChangeLogin({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const MIN_LENGTH = 6;
+    const { email, senha, redirect } = this.state;
     return (
       <>
         <input
@@ -10,6 +34,8 @@ class Login extends React.Component {
           className="input-login"
           placeholder="email"
           data-testid="email-input"
+          onChange={ this.handleChangeLogin }
+          value={ email }
         />
         <input
           type="password"
@@ -17,8 +43,18 @@ class Login extends React.Component {
           className="input-login"
           placeholder="senha"
           data-testid="password-input"
+          onChange={ this.handleChangeLogin }
+          value={ senha }
         />
-        <button type="button" disabled>Entrar</button>
+        {senha.length < MIN_LENGTH ? <button type="button" disabled>Entrar</button> : (
+          <button
+            type="button"
+            onClick={ this.handleClickLogin }
+          >
+            Entrar
+          </button>
+        )}
+        { redirect && <Redirect to="/carteira" /> }
       </>
     );
   }
